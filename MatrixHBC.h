fd8ec9b16f3cff5c92ed36b47416b0ab87b1d80f
Piotr Swat <piotr-swat@valvesoftware.pl>
2019-01-03 19:32:21 +0100

    Matrix H BC implemented

---
+++ MatrixHBC.h
@@ -0,0 +1,25 @@
+//
+// Created by samsung on 1/3/19.
+//
+
+#ifndef FINITEELEMENTMETHOD_MATRIXHBC_H
+#define FINITEELEMENTMETHOD_MATRIXHBC_H
+
+
+#include "Element.h"
+
+class MatrixHBC {
+public:
+    double alpha;
+    double Px[8], Py[8];
+    double matrixH[4][4];
+
+    MatrixHBC();
+    ~MatrixHBC();
+
+    void CalculateMatrixHBC(Element element);
+
+};
+
+
+#endif //FINITEELEMENTMETHOD_MATRIXHBC_H
