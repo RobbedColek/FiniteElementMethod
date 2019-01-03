eb61a47a84eeb39ae51ea37bc04152166678aaae
Piotr Swat <piotr-swat@valvesoftware.pl>
2019-01-03 19:32:34 +0100

    Vector P implemented

---
+++ VectorP.h
@@ -0,0 +1,25 @@
+//
+// Created by samsung on 1/3/19.
+//
+
+#ifndef FINITEELEMENTMETHOD_VECTORP_H
+#define FINITEELEMENTMETHOD_VECTORP_H
+
+#include "Element.h"
+
+
+class VectorP {
+public:
+    double alpha;
+    double Px[8], Py[8];
+    double vectorP[4];
+    double ambientTemperature;
+
+    VectorP();
+    ~VectorP();
+
+    void CalculateVectorP(Element element);
+};
+
+
+#endif //FINITEELEMENTMETHOD_VECTORP_H
